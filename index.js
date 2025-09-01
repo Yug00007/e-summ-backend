a1854f6d4cd87e20a16c8d0de60246d17e2bd659
crocodilelurker <swagatsahu556@gmail.com>
2025-09-02 02:01:44 +0530

    Added Backend DB-  2 Models Defined

--- index.js
+++ index.js
@@ -2,32 +2,40 @@ const express = require('express')
 const cors= require('cors');
  const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
 const getUserInfo = require('./utils/getUserInfo')
- const PORT = process.env.PORT
-const app = express();
 
-app.use(cors()) // default cors until we deploy
+const app = express();
+const connectDb = require('./config/dbConnect.js')
+const dotenv = require("dotenv")
+dotenv.config();
+
+
+const PORT = process.env.PORT
+app.use(cors()) 
 
 app.use(express.urlencoded());
 
-const checkJwt = auth({
-    audience: process.env.audience,
-    issuerBaseURL: process.env.issuerBaseURL,
-});
+connectDb().then();
 
-app.get("/speakers", (req,res)=>{
-    const data = ["dummy"];
-    res.send(data);
-})
 
-app.post("/register", async(req,res)=>{
-    const {username,email, events, contactNo} = req.body
-    const newUser = 'dummy' // db query to create newUser in table after validation
-    if(!newUser){
-    res.status(500).send("Server error, Please contact this no. 2839247483"); // if newUser unsuccessful
-    }
+// const checkJwt = auth({
+//     audience: process.env.audience,
+//     issuerBaseURL: process.env.issuerBaseURL,
+// });
 
-    res.send("success") // newUser created, reflect data on client-side
-})
+// app.get("/speakers", (req,res)=>{
+//     const data = ["dummy"];
+//     res.send(data);
+// })
+
+// app.post("/register", async(req,res)=>{
+//     const {username,email, events, contactNo} = req.body
+//     const newUser = 'dummy' // db query to create newUser in table after validation
+//     if(!newUser){
+//     res.status(500).send("Server error, Please contact this no. 2839247483"); // if newUser unsuccessful
+//     }
+
+//     res.send("success") // newUser created, reflect data on client-side
+// })
 
 // app.post("/login",checkJwt, async(req,res)=>{
 //     const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
@@ -44,4 +52,6 @@ app.post("/register", async(req,res)=>{
 //     res.send({user: userData});
 // })
 
-app.listen(PORT)
+app.listen(PORT,()=>{
+    console.log(`Server Started Running on PORT ${PORT}`);
+})
